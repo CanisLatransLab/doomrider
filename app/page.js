afae949c0ac3ae777d0ed18c58392cd3a2fc3d40
Radek <kojtych.radoslaw@gmail.com>
2024-01-07 22:53:48 +0100

    First version of the doomrider's page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,17 @@
 import Image from 'next/image';
+import wansu from '../public/assets/wonsuHero.jpg';
+import logo from '../public/assets/doom-logo.png';
 
 export default function Home() {
   return (
-    <main>
-      <h1 className='text-6xl'>New DOOMRIDER media web! :D</h1>
+    <main className='bg-[#363636] h-screen grid place-content-center '>
+      <div className='max-w-[50rem]'>
+        <Image src={logo} alt='Doomrider' />
+        <Image src={wansu} alt='Doomrider' />
+      </div>
+      <h1 className=' text-white text-3xl text-center mt-[3vh]'>
+        coming soon ...
+      </h1>
     </main>
   );
 }
